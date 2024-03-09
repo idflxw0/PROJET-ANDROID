2901e196b0b286c9a9aaafe3744438f181a13a71
idfl0wx <idfl0wxzero@gmail.com>
2024-03-09 19:25:16 +0100

    slide

---
+++ slides.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id : '1',
+        title : 'Bienvenue sur Ecorizzz !',
+        description : '',
+        image : require('./assets/LandingImages/fitness_tracker.png')
+    },
+    {
+        id : '2',
+        title : 'Observer votre habitat',
+        description : 'Obtenez une mise à jour dynamique des équipements alloués pour votre habitât et information de consommation ',
+        image : require('./assets/LandingImages/activity.png')
+
+    },
+    {
+        id : '3',
+        title : 'Voir la consommation globale de la résidence',
+        description : 'Affichage sous forme d’un calendrier coloré mettant en valeur les niveaux de consommation globaux',
+        image : require('./assets/LandingImages/stats.png')
+
+    },
+    {
+        id : '4',
+        title : 'Réserver un créneau',
+        description : 'Réservation d’un créneau de la journée pour l’utilisation d’un équipement. Obtenez des eco-coins si vous choisissez des créneaux moins énergivores',
+        image : require('./assets/LandingImages/goal.png')
+
+    },
+]
