e9a0e48e3369a0a964f8e95d4acad1596fca0a44
idfl0wx <idfl0wxzero@gmail.com>
2024-03-09 19:24:51 +0100

    app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LandingPage from "./src/LandingPages/LandingPage";
+import SignInScreen from "./src/LandingPages/ConnectingPage/SignInScreen";
+import SignUp from "./src/LandingPages/ConnectingPage/SignUp";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }}/>
+        <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
