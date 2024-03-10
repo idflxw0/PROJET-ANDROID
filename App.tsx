c4f4d0ef8b0a78941aacd22f3bd9384386757d28
idfl0wx <idfl0wxzero@gmail.com>
2024-03-10 21:02:48 +0100

    App

--- App.tsx
+++ App.tsx
@@ -2,21 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import {createDrawerNavigator} from "@react-navigation/drawer";
+
+//Screens
 import LandingPage from "./src/LandingPages/LandingPage";
 import SignInScreen from "./src/LandingPages/ConnectingPage/SignInScreen";
 import SignUp from "./src/LandingPages/ConnectingPage/SignUp";
-import HomePage from "./src/HomePage";
+import DrawerNavigation from "./src/DrawerNavigation";
 
+//Navigators
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="LandingPage">
         <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }}/>
         <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }}/>
-        <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false }}/>
-
+        <Stack.Screen name="Home" component={DrawerNavigation} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
