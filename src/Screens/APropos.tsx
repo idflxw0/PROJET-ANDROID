3bcea4c0ff70739afb5831e32b0b51de1dfaf837
TMTVato <johnlipro36@gmail.com>
2024-03-11 22:05:13 +0100

    ajout apropos et notif

--- src/Screens/APropos.tsx
+++ src/Screens/APropos.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const APropos = () => {
     return (
         <View style={styles.container}>
-            <Text>
-
-            </Text>
+            <View style={styles.contentContainer}>
+                <Text style={styles.heading}>About</Text>
+                <Text style={styles.content}>
+                    Cette application est une interface utilisateur mobile pour gérer une résidence participative.
+                    Les utilisateurs de cette application sont les résidents de ces habitats.
+                    Chaque habitat a potentiellement accès à une gamme d'appareils ménagers.
+                    La résidence est participative car elle permet aux résidents d'influencer le coût réel de la consommation de la résidence grâce à un système de bonus et de pénalités.
+                    Par exemple, un résident pourrait contribuer à réduire un pic de consommation en s'engageant à n'utiliser qu'une partie de ses appareils ménagers pendant une période définie.
+                    En retour, le résident peut choisir une autre plage horaire moins énergivore et recevoir un bonus dans la monnaie locale de la résidence, appelée l'éco-coin.
+                </Text>
+            </View>
         </View>
     );
 };
@@ -14,10 +22,24 @@ const APropos = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#95E1D3'
-
+        alignItems: 'center',
+        backgroundColor: '#95E1D3',
+    },
+    contentContainer: {
+        marginHorizontal: 20,
+        backgroundColor: '#FFFFFF',
+        padding: 20,
+        borderRadius: 10,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    content: {
+        fontSize: 16,
+        textAlign: 'left',
     },
 });
 
--- src/Screens/Notification.tsx
+++ src/Screens/Notification.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Linking, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from expo/vector-icons
+import { LinearGradient } from 'expo-linear-gradient'; // Import LinearGradient from expo-linear-gradient
 
-const Notification = () => {
+const NotificationScreen = () => {
+    const handleOpenNotificationSettings = async () => {
+        try {
+            if (Platform.OS === 'ios') {
+                await Linking.openURL('app-settings:');
+            } else {
+                await Linking.openSettings();
+            }
+        } catch (error) {
+            console.error('Error opening notification settings:', error);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Text>
-                Notifications
-            </Text>
+
+            <Text style={styles.heading}>Notification Settings</Text>
+            <TouchableOpacity onPress={handleOpenNotificationSettings} style={styles.option}>
+                <Text style={styles.optionText}>Open Notification Settings</Text>
+                <Ionicons name="chevron-forward" size={16} color="black" />
+            </TouchableOpacity>
         </View>
     );
 };
@@ -14,10 +31,37 @@ const Notification = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#95E1D3'
+    },
+    background: {
+        width: '100%',     // Set width to the screen width
+        height: '100%',   // Set height to the screen height
+        position: 'absolute', // Use absolute positioning to overlay
+        top: 0,
+        left: 0
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    option: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '80%',
+        height: 40,
+        borderColor: '#7C7272',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
+    },
+    optionText: {
+        flex: 1,
     },
 });
 
-export default Notification;
+export default NotificationScreen;
